40e2bd7060bd99d131ee03b457e0e47a79ce886e
Kwangho <khseok1060@gmail.com>
2018-05-23 19:32:26 +0900

    Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,22 @@
-const name = "daniel",
-  age = 35,
-  gender = "male";
-
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender: string;
-// }
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-// const person = {
-//   name: "daniel",
-//   age: 35,
-//   gender: "male"
-// };
+const genesisBlock: Block = new Block(0, "223232323", "", "Hello", 123456);
 
-const kh = new Human("kh", 18, "male");
+let blockchain: [Block] = [genesisBlock];
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-
-console.log(sayHi(kh));
+console.log(blockchain);
 
 export {};
