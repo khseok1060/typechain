f11737d539e955bada3dbb4dc0f29e8bff5c5e78
Kwangho <khseok1060@gmail.com>
2018-05-23 19:56:17 +0900

    Creating a Block pt.2

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,17 @@ const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const getNewTimeStamp = (): Number => Math.round(new Date().getTime() / 1000);
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newtHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+  const newBlock: Block = new Block(newIndex, newtHash, previousBlock.hash, data, newTimestamp);
+  return newBlock;
+}
+
+console.log(createNewBlock("Hello"));
 
 export {};
