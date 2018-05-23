8d8f1e81ea9eb8746d71e9e851b2f314a3e110e5
Kwangho <khseok1060@gmail.com>
2018-05-23 20:11:15 +0900

    Validating Block Structure pt.2

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,10 @@ const createNewBlock = (data: string): Block => {
   const newtHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
   const newBlock: Block = new Block(newIndex, newtHash, previousBlock.hash, data, newTimestamp);
   return newBlock;
-}
+};
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
   if (Block.validateStructure(candidateBlock)) {
@@ -52,7 +55,17 @@ const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
     return false;
   } else if (previousBlock.hash !== candidateBlock.previousHash) {
     return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
   }
 };
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+}
 
 export {};
